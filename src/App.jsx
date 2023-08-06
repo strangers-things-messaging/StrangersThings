72e50a86ee9b4a745133819f306c580402a7799e
Jessica Spivak <jls103193@yahoo.com>
2023-08-05 20:32:21 -0400

    created api variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 // import { useState } from 'react'
 import './App.css'
+const COHORT_NAME='2302-acc-et-web-pt-a'
+const API_URL=`https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
+const POSTS_ENDPOINT=`${API_URL}/posts`
 
 function App() {
 
