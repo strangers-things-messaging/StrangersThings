1710e40d3fa27200f9dca93c1276fdcd42cfe7e2
Jessica Spivak <jls103193@yahoo.com>
2023-10-17 10:39:07 -0400

    some styling added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,20 +10,48 @@ export default function NavBar({ token, setToken }) {
     }
     if (!token) {
         return (
-            <div id='navbar' >
-                <Link to="/">Sign Up</Link>
-                <Link to="/LoginPage">Login</Link>
-                <Link to="/Posts">Posts</Link>
-            </div>
+            <>
+                <div className="navDiv">
+                <nav className="navbar fixed-top bg-body-tertiary navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid navColor">
+                        <a className="navbar-brand" href="#">Stranger's Things</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div className="navbar-nav">
+                                <Link to="/" className="nav-link"  href="#">Sign Up</Link>
+                                <Link to="/LoginPage" className="nav-link" href="#">Log In</Link>
+                                <Link to="/Posts" className="nav-link" href="#">Posts</Link>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+                </div>
+            </>
         )
     } else if (token) {
         return (
-            <div id='navbar' >
-                <Link to="/ProfilePage">Profile Page</Link>
-                <Link to="/CreatePostPage">Create Post</Link>
-                <Link to="/Posts">Posts</Link>
-                <button onClick={handleClick}>Logout</button>
-            </div>
+            <>
+                <div className="navDiv">
+                <nav className="navbar fixed-top bg-body-tertiary navbar-expand-lg bg-body-tertiary " >
+                    <div className="container-fluid navColor">
+                        <a className="navbar-brand" href="#">Stranger's Things</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div className="navbar-nav">
+                                <Link to="/ProfilePage" className="nav-link"  href="#">Profile</Link>
+                                <Link to="/CreatePostPage" className="nav-link" href="#">Create Post</Link>
+                                <Link to="/Posts" className="nav-link" href="#">Posts</Link>
+                                <button className="nav-link" onClick={handleClick}>Logout</button>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+                </div>
+            </>
         )
     }
 
